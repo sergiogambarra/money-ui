fd3d57071ac65e6a8ded8e8f06b8013224a7725a
Sergio Gambarra <sergio@gambarra.com.br>
2020-11-28 00:54:16 -0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://sergio-money-api.herokuapp.com',
 
-  tokenAllowedDomains: [ new RegExp('algamoney-api.herokuapp.com') ],
+  tokenAllowedDomains: [ new RegExp('sergio-money-api.herokuapp.com') ],
   tokenDisallowedRoutes: [ new RegExp('\/oauth\/token') ]
 };
